9b3ae4ef7a110f7b06705ef19a4ed86d1e2d5df1
TomDotBat <tom@tomdotbat.dev>
2020-11-11 20:16:05 +0000

    Serverside GM initialisation

---
+++ gamemode/init.lua
@@ -0,0 +1,30 @@
+
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+include("shared.lua")
+
+function GM:Initialize()
+    self.Sandbox.Initialize(self)
+end
+
+function GM:InitPostEntity()
+    self.InitPostEntityCalled = true
+
+    local physData = physenv.GetPerformanceSettings()
+    physData.MaxVelocity = 2000
+    physData.MaxAngularVelocity = 3636
+    physenv.SetPerformanceSettings(physData)
+
+    game.ConsoleCommand("sv_allowcslua 0\n")
+    game.ConsoleCommand("physgun_DampingFactor 0.9\n")
+    game.ConsoleCommand("sv_sticktoground 0\n")
+    game.ConsoleCommand("sv_airaccelerate 1000\n")
+    game.ConsoleCommand("sv_alltalk 0\n")
+end
+
+timer.Simple(0.1, function()
+    if not GAMEMODE.InitPostEntityCalled then
+        GAMEMODE:InitPostEntity()
+    end
+end)
