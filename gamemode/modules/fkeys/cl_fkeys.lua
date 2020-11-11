a07d60fe63b510f42e18f5d09091e42d1f079c48
TomDotBat <tom@tomdotbat.dev>
2020-11-11 20:24:07 +0000

    Function key hooks

---
+++ gamemode/modules/fkeys/cl_fkeys.lua
@@ -0,0 +1,61 @@
+
+local keyBinds = {
+    ["gm_showhelp"] = "ShowHelp",
+    ["gm_showteam"] = "ShowTeam",
+    ["gm_showspare1"] = "ShowSpare1",
+    ["gm_showspare2"] = "ShowSpare2"
+}
+
+function GM:PlayerBindPress(ply, bind, pressed)
+    self.Sandbox.PlayerBindPress(self, ply, bind, pressed)
+    bind = string.lower(bind)
+
+    local bnd = string.match(bind, "gm_[a-z]+[12]?")
+    if bnd and keyBinds[bnd] then
+        hook.Call(keyBinds[bnd], GAMEMODE)
+    end
+
+    if not RPGM.Config.VoiceWhenDead and not ply:Alive() and string.find(bind, "voicerecord") then return true end
+end
+
+local function handleFKeyPress(ply, keyNo)
+    if ply ~= RPGM.Util.GetLocalPlayer() then return end
+    if keyNo < 0 or keyNo > 4 then return end
+
+    hook.Call("RPGM.FKeyPressed", GAMEMODE, keyNo)
+end
+
+function GM:ShowHelp(ply)
+    handleFKeyPress(ply, 1)
+end
+
+function GM:ShowTeam(ply)
+    handleFKeyPress(ply, 2)
+end
+
+function GM:ShowSpare1(ply)
+    handleFKeyPress(ply, 3)
+end
+
+function GM:ShowSpare2(ply)
+    handleFKeyPress(ply, 4)
+end
+
+local mouseX, mouseY
+local cursorUnlocked = false
+
+hook.Add("RPGM.FKeyPressed", "RPGM.UnlockCursorBind", function(keyNo)
+    cursorUnlocked = not cursorUnlocked
+
+    if cursorUnlocked then
+        if not (mouseX and mouseY) then
+            gui.SetMousePos(ScrW() / 2, ScrH() / 2)
+        else
+            gui.SetMousePos(mouseX, mouseY)
+        end
+    else
+        mouseX, mouseY = gui.MousePos()
+    end
+
+    gui.EnableScreenClicker(cursorUnlocked)
+end)
