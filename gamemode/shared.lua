5481c260c1d1f5fff20324c0924f4147abb37929
TomDotBat <tom@tomdotbat.dev>
2020-11-11 20:16:24 +0000

    Add module loader

---
+++ gamemode/shared.lua
@@ -0,0 +1,43 @@
+
+GM.Name = "RPGM: An RP Gamemode Base"
+GM.Author = "Tom.bat"
+GM.Email = "tom@tomdotbat.dev"
+GM.Website = "https://tomdotbat.dev"
+
+DeriveGamemode("sandbox")
+DEFINE_BASECLASS("gamemode_sandbox")
+
+GM.Sandbox = BaseClass
+
+RPGM = RPGM or {
+    Config = {},
+    Util = {}
+}
+
+function RPGM.Util.LoadDirectory(dir)
+    local files, folders = file.Find(dir .. "/*", "LUA")
+
+    for k, v in pairs(files) do
+        local dirPath = dir .. "/" .. v
+
+        if v:StartWith("cl_") then
+            if SERVER then AddCSLuaFile(dirPath)
+            else include(dirPath) end
+        elseif v:StartWith("sh_") then
+            if SERVER then AddCSLuaFile(dirPath) end
+            include(dirPath)
+        else
+            if CLIENT then continue end
+            include(dirPath)
+        end
+    end
+
+    for k, v in pairs(folders) do
+        RPGM.Util.LoadDirectory(dir .. "/" .. v)
+    end
+end
+
+local rootDir = GM.FolderName .. "/gamemode/"
+RPGM.Util.LoadDirectory(rootDir .. "config")
+RPGM.Util.LoadDirectory(rootDir .. "libraries")
+RPGM.Util.LoadDirectory(rootDir .. "modules")
