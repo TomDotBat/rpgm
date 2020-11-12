1b0ea8cc1475b44f609b252f02e0998776ff9125
TomDotBat <tom@tomdotbat.dev>
2020-11-12 12:25:49 +0000

    Item extra registering

---
+++ gamemode/modules/core/classes/sv_item_extras.lua
@@ -0,0 +1,45 @@
+
+local extras = {}
+
+function RPGM.Classes.RegisterExtra(classType, varName, useAccessors, getter, setter)
+    if not RPGM.Classes[classType] then
+        print("Warning! Invaliad class type specified when registering an extra value.")
+        debug.Trace()
+        return
+    end
+
+    if not extras[classType] then extras[classType] = {} end
+
+    if not useAccessors then
+        extras[classType][varName] = true
+        return
+    end
+
+    extras[classType][varName] = {getter, setter}
+end
+
+function RPGM.Classes.SetupExtras(class)
+    local classExtras = extras[class.__type]
+    if not classExtras then return end
+
+    local extraTable = class:getExtra()
+
+    for k,v in pairs(classExtras) do
+        local accessorData = extras[classType]
+        if isbool(accessorData) then continue end
+
+        local accessorName = k:gsub("^%l", string.upper)
+
+        if accessorData[1] then
+            class["get" .. accessorName] = accessorData[1]
+        else
+            class["get" .. accessorName] = function(self) return extraTable[k] end
+        end
+
+        if accessorData[2] then
+            class["set" .. accessorName] = accessorData[2]
+        else
+            class["set" .. accessorName] = function(self, val) extraTable[k] = val end
+        end
+    end
+end
