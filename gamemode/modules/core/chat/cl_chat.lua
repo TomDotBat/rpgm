af7f3566e448a8f49b150da37e7e43ce48878a80
TomDotBat <tom@tomdotbat.dev>
2020-11-18 19:27:49 +0000

    Chat message receivers

---
+++ gamemode/modules/core/chat/cl_chat.lua
@@ -0,0 +1,75 @@
+
+function GM:OnPlayerChat() end
+
+local function safeText(text)
+    return string.match(text, "^#([a-zA-Z_]+)$") and text .. " " or text
+end
+
+local messageColor = RPGM.Config.ChatMessageColor
+local deadMessageColor = RPGM.Config.ChatMessageDeadColor
+local getTeamColor = team.GetColor
+
+net.Receive("RPGM.Talk", function()
+    local talker = net.ReadEntity()
+    if not IsValid(talker) then return end
+
+    local text = safeText(net.ReadString())
+    local talkerDead = net.ReadBool()
+
+    local talkerName
+    if net.BytesLeft() == 0 then
+        talkerName = talker:Nick()
+    else
+        local override = net.ReadString()
+        if isstring(override) and override != "" then
+            talkerName = override
+        end
+    end
+
+    hook.Call("OnPlayerChat", GAMEMODE, talker, text, false, talkerDead)
+
+    if net.BytesLeft() != 0 then
+        chat.AddText(
+            net.ReadColor(), net.ReadString() .. " ",
+            talkerDead and deadMessageColor or getTeamColor(talker:Team()), talkerName,
+            color_white, ": ",
+            talkerDead and deadMessageColor or messageColor, text
+        )
+
+        return
+    end
+
+    chat.AddText(
+        talkerDead and deadMessageColor or getTeamColor(talker:Team()), talkerName,
+        color_white, ": ",
+        talkerDead and deadMessageColor or messageColor, text
+    )
+
+    chat.PlaySound()
+end)
+
+local defaultPrefixCol = Color(52, 168, 235)
+
+net.Receive("RPGM.Chat", function()
+    local text = net.ReadString()
+
+    local textColor = net.ReadColor()
+    if not IsColor(textColor) then textColor = messageColor end
+
+    if net.BytesLeft() != 0 then
+        local prefixCol, prefix = net.ReadColor(), net.ReadString()
+
+        chat.AddText(
+            prefixCol, prefix .. " ",
+            textColor, text
+        )
+        MsgC(defaultPrefixCol, "[RPGM] ", textColor, text)
+
+        return
+    end
+
+    chat.AddText(textColor, text)
+    MsgC(prefixCol, prefix .. " ", textColor, text)
+
+    chat.PlaySound()
+end)
