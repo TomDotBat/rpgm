51b111433e73826db7996dd9a4c8bb92648d11b8
TomDotBat <tom@tomdotbat.dev>
2020-11-18 13:51:54 +0000

    Fix potential error with chat messages

--- gamemode/modules/core/chat/sv_chat.lua
+++ gamemode/modules/core/chat/sv_chat.lua
@@ -1,8 +1,10 @@
 
 function GM:PlayerSay(ply, text)
-    --range checks here pls dad
-
     if string.StartWith(text, RPGM.Config.ChatCommandPrefix) and RPGM.HandleCommands(ply, string.Right(text, #text - 1)) then
         return ""
     end
+
+    --range checks here pls dad
+
+    return text
 end
