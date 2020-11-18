deade195c807e1619b70470a724cbb363cf25221
TomDotBat <tom@tomdotbat.dev>
2020-11-18 19:27:09 +0000

    Chat message colours

--- gamemode/config/cl_config.lua
+++ gamemode/config/cl_config.lua
@@ -2,5 +2,9 @@
 --Death Settings
 RPGM.Config.FirstPersonDeathView = true
 
+--Chat Settings
+RPGM.Config.ChatMessageColor = Color(255, 255, 255, 255)
+RPGM.Config.ChatMessageDeadColor = Color(255, 200, 200, 255)
+
 --Misc Settings
 RPGM.Config.EnableDecalCleaner = true 
