35e8c91fb53c3be88fd330b436f23e4962a2526b
TomDotBat <tom@tomdotbat.dev>
2020-11-19 15:51:30 +0000

    Player changed team hook and networking

---
+++ gamemode/modules/core/classes/teams/sv_hooks.lua
@@ -0,0 +1,14 @@
+
+local getTeamClass = team.GetClass
+
+function GM:PlayerChangedTeam(ply, oldTeamId, newTeamId)
+    RPGM.CallClassFunction(getTeamClass(oldTeamId), "onPlayerLeave", ply, newTeamId)
+    RPGM.CallClassFunction(getTeamClass(newTeamId), "onPlayerJoin", ply, oldTeamId)
+
+    net.Start("RPGM.TeamChanged")
+     net.WriteUInt(oldTeamId, 10)
+     net.WriteUInt(newTeamId, 10)
+    net.Send(ply)
+end
+
+util.AddNetworkString("RPGM.TeamChanged")
