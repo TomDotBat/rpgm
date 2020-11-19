459f7402ba16e4f83d40280c93454817bf4c78da
TomDotBat <tom@tomdotbat.dev>
2020-11-19 18:17:56 +0000

    Implement better model getter

--- gamemode/modules/core/spawn/sv_set_model.lua
+++ gamemode/modules/core/spawn/sv_set_model.lua
@@ -1,6 +1,21 @@
 
 function GM:PlayerSetModel(ply)
-    ply:SetModel("models/player/charple.mdl")
+    local teamTbl = ply:getTeamClass()
+    if not teamTbl then return self.Sandbox.PlayerSetModel(self, ply) end
+
+    local model = RPGM.CallClassFunction(teamTbl, "setModel", ply)
+    if model then
+        ply:SetModel(model)
+        return
+    end
+
+    if RPGM.Config.ForcePlayerModel then
+        model = teamTbl:getModel()
+    else
+        model = player_manager.TranslatePlayerModel(ply:GetInfo("cl_playermodel"))
+    end
+
+    ply:SetModel(model)
     self.Sandbox.PlayerSetModel(self, ply)
     ply:SetupHands()
 end
