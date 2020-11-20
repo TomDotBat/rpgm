87c04e1a8103b54a94f254b1d92fe56c728417fd
TomDotBat <tom@tomdotbat.dev>
2020-11-20 20:57:26 +0000

    Clientside team networking

--- gamemode/modules/core/classes/teams/cl_networking.lua
+++ gamemode/modules/core/classes/teams/cl_networking.lua
@@ -3,3 +3,27 @@ net.Receive("RPGM.TeamChanged", function()
     local oldTeam, newTeam = net.ReadUInt(10), net.ReadUInt(10)
     hook.Call("OnLocalTeamChanged", GAMEMODE, oldTeam, newTeam)
 end)
+
+net.Receive("RPGM.DownloadTeams", function()
+    local len = net.ReadUInt(32)
+    local teamData = net.ReadData(len)
+
+    teamData = util.Decompress(teamData)
+    teamData = RPGM.MessagePack.Unpack(teamData)
+
+    RPGM.SetupTeams(teamData)
+end)
+
+net.Receive("RPGM.DownloadTeam", function()
+    local len = net.ReadUInt(32)
+    local teamData = net.ReadData(len)
+
+    teamData = util.Decompress(teamData)
+    teamData = RPGM.MessagePack.Unpack(teamData)
+
+    RPGM.SetupTeam(teamData)
+end)
+
+net.Receive("RPGM.DeleteTeam", function()
+    RPGM.RemoveTeam(net.ReadString())
+end)
