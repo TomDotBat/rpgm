69bf53ae38ec481b2f24117fdfeb76eacea22273
TomDotBat <tom@tomdotbat.dev>
2020-11-20 18:42:49 +0000

    Set category attribute defaults and allow partial creation

--- gamemode/modules/core/classes/sh_categories.lua
+++ gamemode/modules/core/classes/sh_categories.lua
@@ -4,10 +4,11 @@ local categories = {}
 function RPGM.AddCategory(classType, name, color, order)
     if not name then return end
     if not categories[classType] then categories[classType] = {} end
+    if categories[classType][name] and not table.IsEmpty(categories[classType][name]) then return end
 
     categories[classType][name] = {
-        color = color,
-        order = order
+        color = color or color_white,
+        order = order or 0
     }
 end
 
