59cd73290488ffe296c7ca97d5be8717a21e9a3f
Tom <tom@tomdotbat.dev>
2020-11-24 22:56:22 +0000

    Notify the player when they're not allowed to become a certain team

--- gamemode/modules/core/classes/teams/sv_team_change.lua
+++ gamemode/modules/core/classes/teams/sv_team_change.lua
@@ -6,11 +6,18 @@ function RPGM.JoinTeam(ply, cmd, force)
     if not force then
         local limit = teamTbl:getLimit(ply)
         if limit > 0 and team.NumPlayers(teamTbl.__id) >= limit then
-
+            ply:rpNotify("There are no more slots available for " .. teamTbl:getName() .. ".", NOTIFY_ERROR)
             return
         end
 
-        if not teamTbl:doCustomCheck(ply) then
+        local allowed, reason = teamTbl:doCustomCheck(ply)
+        if not allowed then
+            if reason then
+                ply:rpNotify(reason, NOTIFY_ERROR)
+                return
+            end
+
+            ply:rpNotify("You don't meet the requirements to join " .. teamTbl:getName() .. ".", NOTIFY_ERROR)
             return
         end
     end
