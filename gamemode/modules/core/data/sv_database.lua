318971910573b146338fcc03453d6033ab0e0e80
TomDotBat <tom@tomdotbat.dev>
2020-11-24 18:51:51 +0000

    Database initializer

---
+++ gamemode/modules/core/data/sv_database.lua
@@ -0,0 +1,32 @@
+
+function RPGM.InitializeDB()
+    MySQLite.initialize({
+        EnableMySQL = RPGM.Config.Database.EnableMySQL,
+        Host        = RPGM.Config.Database.Host,
+        Username    = RPGM.Config.Database.Username,
+        Password    = RPGM.Config.Database.Password,
+        Database_name = RPGM.Config.Database.Database,
+        Preferred_module = "mysqloo",
+        MultiStatements = false
+    })
+
+    local isMySQL = MySQLite.isMySQL()
+    local autoIncrement = isMySQL and "AUTO_INCREMENT" or "AUTOINCREMENT"
+    local tablePrefix = RPGM.Config.Database.TablePrefix
+
+    MySQLite.begin()
+
+    MySQLite.queueQuery("CREATE TABLE IF NOT EXISTS " .. tablePrefix .. "version(version INTEGER NOT NULL PRIMARY KEY)")
+
+    MySQLite.queueQuery("SELECT MAX(version) AS version FROM " .. tablePrefix .. "version", function(data)
+        RPGM.DBVersion = data and data[1] and tonumber(data[1].version) or 1
+    end)
+
+    MySQLite.queueQuery("REPLACE INTO " .. tablePrefix .. "version VALUES(1)")
+
+    hook.Call("DBBuilder", GM or GAMEMODE, autoIncrement)
+
+    MySQLite.commit(function()
+        hook.Call("DBInitialized", GM or GAMEMODE, RPGM.DBVersion, isMySQL, tablePrefix)
+    end)
+end
