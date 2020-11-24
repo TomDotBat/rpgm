088e7f68c2190aa67e8c1c8a0b153d00c6f4c1bc
Tom <tom@tomdotbat.dev>
2020-11-24 23:53:06 +0000

    Fix metamethod function calls

--- gamemode/modules/money/sh_meta.lua
+++ gamemode/modules/money/sh_meta.lua
@@ -5,7 +5,7 @@ function meta:getMoney(callback)
     if CLIENT then
         if callback then callback(self:getRPInt("Money", -1)) end
     else
-        if callback then RPGM.GetPlayerPoints(self, callback) end
+        if callback then RPGM.GetPlayerMoney(self, callback) end
     end
 
     return self:getRPInt("Money", -1)
@@ -15,7 +15,7 @@ function meta:canAfford(amount, callback)
     if CLIENT then
         if callback then callback(amount <= self:getRPInt("Money", -1)) end
     else
-        if callback then RPGM.CanPlayerAffordPoints(self, amount, callback) end
+        if callback then RPGM.CanPlayerAffordMoney(self, amount, callback) end
     end
 
     return amount <= self:getRPInt("Money", -1)
@@ -24,13 +24,13 @@ end
 if CLIENT then return end
 
 function meta:setMoney(amount, callback)
-    RPGM.SetPlayerPoints(self, amount, callback)
+    RPGM.SetPlayerMoney(self, amount, callback)
 end
 
 function meta:addMoney(amount, callback)
-    RPGM.GivePlayerPoints(self, amount, callback)
+    RPGM.GivePlayerMoney(self, amount, callback)
 end
 
 function meta:removeMoney(amount, callback)
-    RPGM.RemovePlayerPoints(self, amount, callback)
+    RPGM.RemovePlayerMoney(self, amount, callback)
 end
