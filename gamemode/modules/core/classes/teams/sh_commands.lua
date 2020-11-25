4e1016cc3440266a94ac2ce90543364da8278a5b
TomDotBat <tom@tomdotbat.dev>
2020-11-25 19:21:30 +0000

    Give the player a message when they try to join the same team

--- gamemode/modules/core/classes/teams/sh_commands.lua
+++ gamemode/modules/core/classes/teams/sh_commands.lua
@@ -8,6 +8,14 @@ hook.Add("RPGM.RegisterCommands", "RPGM.TeamCommand", function()
         local cmd = data[1]
         if not cmd then return end
 
+        cmd = string.lower(cmd)
+
+        local teamTbl = ply:getTeamClass()
+        if teamTbl and cmd == teamTbl:getCommand() then
+            RPGM.MessagePlayer(ply, "You are already a \"" .. cmd .. "\".", errorMessageCol)
+            return
+        end
+
         if ply:joinTeam(cmd) then
             RPGM.MessagePlayer(ply, "The team \"" .. cmd .. "\" couldn't be found.", errorMessageCol)
         end
