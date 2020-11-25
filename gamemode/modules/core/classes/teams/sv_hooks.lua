3999b84f5ddadef18f7749f8bb94a67c93b6912b
TomDotBat <tom@tomdotbat.dev>
2020-11-25 18:44:52 +0000

    Prevent notifying players of a first team change

--- gamemode/modules/core/classes/teams/sv_hooks.lua
+++ gamemode/modules/core/classes/teams/sv_hooks.lua
@@ -2,13 +2,12 @@
 util.AddNetworkString("RPGM.TeamChanged")
 
 function GM:PlayerChangedTeam(ply, oldTeamId, newTeamId)
-    if oldTeamId then
-        ply.lastTeam = oldTeamId
-        RPGM.CallClassFunction(RPGM.TeamTableID[oldTeamId], "onPlayerLeave", ply, newTeamId)
-    end
-
     local newTeam = RPGM.TeamTableID[newTeamId]
     RPGM.CallClassFunction(newTeam, "onPlayerJoin", ply, oldTeamId)
+
+    if not oldTeamId then return end
+    ply.lastTeam = oldTeamId
+    RPGM.CallClassFunction(RPGM.TeamTableID[oldTeamId], "onPlayerLeave", ply, newTeamId)
 
     net.Start("RPGM.TeamChanged")
      net.WriteUInt(oldTeamId, 10)
