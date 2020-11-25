814a05f1fce2059df382bb637d26060cced116b4
TomDotBat <tom@tomdotbat.dev>
2020-11-25 20:46:57 +0000

    Fix serverside notification error

--- gamemode/modules/core/notify/sh_meta.lua
+++ gamemode/modules/core/notify/sh_meta.lua
@@ -4,6 +4,7 @@ local meta = FindMetaTable("Player")
 function meta:rpNotify(text, type, len, disableSound)
     if SERVER then
         RPGM.Notify(self, text, type, len)
+        return
     end
 
     RPGM.Notify(text, type, len, disableSound)
