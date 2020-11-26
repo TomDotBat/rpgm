f01174b304bdde4a8fb27126a90ebefcc2f1dd6a
TomDotBat <tom@tomdotbat.dev>
2020-11-26 11:28:47 +0000

    HUD drawing hook

--- gamemode/modules/core/util/cl_util.lua
+++ gamemode/modules/core/util/cl_util.lua
@@ -4,3 +4,13 @@ function RPGM.Util.GetLocalPlayer()
     if localPly == NULL then localPly = LocalPlayer() end
     return localPly
 end
+
+local scrW, scrH = ScrW(), ScrH()
+hook.Add("OnScreenSizeChanged", "RPGM.StoreResolution", function()
+    scrW, scrH = ScrW(), ScrH()
+end)
+
+local callHook = hook.Call
+function GM:HUDPaint()
+    callHook("RPGM.DrawHUD", nil, scrW, scrH)
+end
