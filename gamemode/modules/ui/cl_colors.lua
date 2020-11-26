1386f37e4c874a67d68fa7db1f3a2084b48b5c3f
TomDotBat <tom@tomdotbat.dev>
2020-11-26 20:18:28 +0000

    Add a global colour table

---
+++ gamemode/modules/ui/cl_colors.lua
@@ -0,0 +1,8 @@
+
+RPGM.Colors = {}
+
+RPGM.Colors.Background = Color(33, 33, 33, 253)
+RPGM.Colors.Header = Color(82, 87, 93, 90)
+
+RPGM.Colors.Primary = Color(255, 255, 255, 255)
+RPGM.Colors.Secondary = Color(189, 189, 189, 255)
