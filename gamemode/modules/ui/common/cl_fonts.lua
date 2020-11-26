fdb332cae6d44bc8fef89d4fb78b24176c9cea9d
TomDotBat <tom@tomdotbat.dev>
2020-11-26 10:51:12 +0000

    Font registration helpers

---
+++ gamemode/modules/ui/common/cl_fonts.lua
@@ -0,0 +1,33 @@
+
+local scrh = ScrH
+local max = math.max
+function RPGM.Scale(value)
+    return max(value * (scrh() / 1080), 1)
+end
+
+function RPGM.RegisterFontUnscaled(name, font, size, weight)
+    surface.CreateFont("RPGM." .. name, {
+        font = font,
+        size = size,
+        weight = weight or 500,
+        antialias = true
+    })
+end
+
+local scaledFonts = {}
+
+function RPGM.RegisterFont(name, font, size, weight)
+    scaledFonts[name] = {
+        font = font,
+        size = size,
+        weight = weight
+    }
+
+    RPGM.RegisterFontUnscaled(name, font, RPGM.Scale(size), weight)
+end
+
+hook.Add("OnScreenSizeChanged", "RPGM.ReRegisterFonts", function()
+    for k,v in pairs(scaledFonts) do
+        RPGM.RegisterFont(k, v.font, v.size, v.weight)
+    end
+end)
