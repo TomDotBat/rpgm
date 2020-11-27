3b1416fb8b56d5f359b365e1f5e08729981b7ccd
TomDotBat <tom@tomdotbat.dev>
2020-11-27 17:25:08 +0000

    Primary and negative colours

--- gamemode/modules/ui/cl_colors.lua
+++ gamemode/modules/ui/cl_colors.lua
@@ -4,5 +4,8 @@ RPGM.Colors = {}
 RPGM.Colors.Background = Color(33, 33, 33, 253)
 RPGM.Colors.Header = Color(82, 87, 93, 90)
 
-RPGM.Colors.Primary = Color(255, 255, 255, 255)
-RPGM.Colors.Secondary = Color(189, 189, 189, 255)
+RPGM.Colors.PrimaryText = Color(255, 255, 255, 255)
+RPGM.Colors.SecondaryText = Color(189, 189, 189, 255)
+
+RPGM.Colors.Primary = Color(25, 118, 210)
+RPGM.Colors.Negative = Color(183, 28, 28)
