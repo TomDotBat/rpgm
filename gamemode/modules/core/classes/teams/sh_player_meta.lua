9d1385e5f6263dd7bab9a1605102d858ba2adccf
TomDotBat <tom@tomdotbat.dev>
2020-11-27 17:25:28 +0000

    Team name getter metamethod

--- gamemode/modules/core/classes/teams/sh_player_meta.lua
+++ gamemode/modules/core/classes/teams/sh_player_meta.lua
@@ -4,3 +4,7 @@ local meta = FindMetaTable("Player")
 function meta:getTeamClass()
     return RPGM.TeamTableID[self:Team()]
 end
+
+function meta:getTeamName()
+    return team.GetName(self:Team())
+end
