4608160abd177eea414fe9a62bf2f067cf53cc9e
TomDotBat <tom@tomdotbat.dev>
2020-11-28 17:47:28 +0000

    Set up the default types (in case running w/out sandbox)

--- gamemode/modules/core/notify/sh_types.lua
+++ gamemode/modules/core/notify/sh_types.lua
@@ -1,4 +1,10 @@
 
+NOTIFY_GENERIC = 0
+NOTIFY_ERROR = 1
+NOTIFY_UNDO = 2
+NOTIFY_HINT = 3
+NOTIFY_CLEANUP = 4
+
 RPGM.NotificationTypes = RPGM.NotificationTypes or {
     [NOTIFY_GENERIC] = {"Information", "rTZyn4X"},
     [NOTIFY_ERROR] = {"Error", "xbMow0M"},
