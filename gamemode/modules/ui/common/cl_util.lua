1a06c6adb585e7e6c4bc4ea21c67bc40428be22b
TomDotBat <tom@tomdotbat.dev>
2020-12-03 16:25:36 +0000

    Fix the rotated rect drawing function

--- gamemode/modules/ui/common/cl_util.lua
+++ gamemode/modules/ui/common/cl_util.lua
@@ -3,11 +3,11 @@ local rad = math.rad
 local sin, cos = math.sin, math.cos
 function RPGM.DrawTexturedRectRotatedPoint(x, y, w, h, rot, x0, y0)
     local ang = rad(rot)
-    local sinAng = sin(ang)
+    local sinAng, cosAng = sin(ang), cos(ang)
 
     surface.DrawTexturedRectRotated(
-        x + y0 * sinAng - x0 * cos,
-        y + y0 * cos(ang) + x0 * sinAng,
+        x + y0 * sinAng - x0 * cosAng,
+        y + y0 * cosAng + x0 * sinAng,
         w, h, rot
     )
 end
