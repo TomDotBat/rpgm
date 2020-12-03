5c3f8f5c308bfd973cc5a87a0aea17b9c13c4b2a
TomDotBat <tom@tomdotbat.dev>
2020-12-03 17:36:52 +0000

    Helper for drawing dumb trimmed textures (thx billy)

--- gamemode/modules/ui/common/cl_util.lua
+++ gamemode/modules/ui/common/cl_util.lua
@@ -11,3 +11,12 @@ function RPGM.DrawTexturedRectRotatedPoint(x, y, w, h, rot, x0, y0)
         w, h, rot
     )
 end
+
+local du = 0.5 / 32
+local dv = 0.5 / 32
+local u0, v0 = (0 - du) / (1 - 2 * du), (0 - dv) / (1 - 2 * dv)
+local u1, v1 = (1 - du) / (1 - 2 * du), (1 - dv) / (1 - 2 * dv)
+function RPGM.DrawSubpixelClippedMaterial(mat, x, y, w, h)
+    surface.SetMaterial(mat)
+    surface.DrawTexturedRectUV(x, y, w, h, u0, v0, u1, v1)
+end
