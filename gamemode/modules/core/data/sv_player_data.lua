b4842bc2553c934d2d61636829124fcb5615ce92
TomDotBat <tom@tomdotbat.dev>
2020-12-06 16:02:45 +0000

    Store player steam name and nickname

--- gamemode/modules/core/data/sv_player_data.lua
+++ gamemode/modules/core/data/sv_player_data.lua
@@ -3,27 +3,30 @@ local tableName = RPGM.Config.Database.TablePrefix .. "players"
 
 hook.Add("RPGM.DBBuilder", "RPGM.BuildPlayerTable", function()
     MySQLite.queueQuery([[CREATE TABLE IF NOT EXISTS "]] .. tableName .. [[" (
-	steamid	TEXT NOT NULL,
-	name	TEXT NOT NULL,
+	steamid	    TEXT NOT NULL,
+	name	    TEXT NOT NULL,
+    steam_name	TEXT NOT NULL,
 	PRIMARY KEY("steamid")
 );]])
 end)
 
-function RPGM.SetPlayerNameInDB(steamid, name, callback)
+function RPGM.SetPlayerNameInDB(steamid, name, steamName, callback)
     RPGM.CheckType(steamid, "string")
     RPGM.CheckType(name, "string")
+    RPGM.CheckType(steamName, "string")
 
     name = MySQLite.SQLStr(name)
+    steamName = MySQLite.SQLStr(steamName)
 
     local query = [[INSERT INTO `]] .. tableName
-        .. [[` (steamid, name) VALUES("]] .. steamid .. [[", "]] .. name .. [[")]]
+        .. [[` (steamid, name, steam_name) VALUES("]] .. steamid .. [[", "]] .. name .. [[", "]] .. steamName .. [[")]]
 
     if MySQLite.isMySQL() then
         query = query
-            .. [[ ON DUPLICATE KEY UPDATE name = "]] .. name .. [[";]]
+            .. [[ ON DUPLICATE KEY UPDATE name = "]] .. name .. [[", steam_name = "]] .. steamName .. [[";]]
     else
         query = query
-            .. [[ ON CONFLICT(steamid) DO UPDATE SET name = "]] .. name .. [[";]]
+            .. [[ ON CONFLICT(steamid) DO UPDATE SET name = "]] .. name .. [[", steam_name = "]] .. steamName .. [[";]]
     end
 
     MySQLite.query(
@@ -37,5 +40,5 @@ function RPGM.SetPlayerNameInDB(steamid, name, callback)
 end
 
 hook.Add("PlayerInitialSpawn", "RPGM.InitialisePlayerData", function(ply)
-    RPGM.SetPlayerNameInDB(ply:SteamID64(), ply:Nick())
+    RPGM.SetPlayerNameInDB(ply:SteamID64(), ply:Nick(), ply.SteamName and ply:SteamName() or ply:Name())
 end)
