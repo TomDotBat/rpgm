a5dbb08247712a6f116bf206739b06dc5161a4d5
TomDotBat <tom@tomdotbat.dev>
2020-12-12 17:27:22 +0000

    Prevent team related loadout errors

--- gamemode/modules/core/spawn/sv_loadout.lua
+++ gamemode/modules/core/spawn/sv_loadout.lua
@@ -2,13 +2,14 @@
 function GM:PlayerLoadout(ply)
     self.Sandbox.PlayerLoadout(self, ply)
 
-    local teamTbl = ply:getTeamClass()
-    RPGM.CallClassFunction(teamTbl, "onPlayerLoadout", ply)
-
     for k, v in pairs(RPGM.Config.BaseLoadout) do
         ply:Give(v)
     end
 
+    local teamTbl = ply:getTeamClass()
+    if not teamTbl then return end
+
+    RPGM.CallClassFunction(teamTbl, "onPlayerLoadout", ply)
     for k,v in ipairs(teamTbl:getWeapons()) do
         ply:Give(v)
     end
