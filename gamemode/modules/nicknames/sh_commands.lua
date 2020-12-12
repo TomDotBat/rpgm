e15475699878a2ee11ddc63ba47d71520c63b541
TomDotBat <tom@tomdotbat.dev>
2020-12-12 16:04:47 +0000

    Self nickname reset command

--- gamemode/modules/nicknames/sh_commands.lua
+++ gamemode/modules/nicknames/sh_commands.lua
@@ -6,15 +6,16 @@ hook.Add("RPGM.RegisterCommands", "RPGM.NicknameCommands", function()
         RPGM.ChangeNickname(ply, data[1], ply)
     end)
 
-    RPGM.RegisterCommand("resetnick", {
-        "resetrpname", "resetname", "resetnickname",
-        "removenick", "removerpname", "removename", "removenickname",
-        "deletenick", "deleterpname", "deletename", "deletenickname",
-        "clearnick", "clearrpname", "clearname", "clearnickname",
+    RPGM.RegisterCommand("clearnick", {
+        "clearrpname", "clearname", "clearnickname",
         "freenick", "freerpname", "freename", "freenickname"
     }, {
         RPGM.Classes.TextArgument("Name", false, nil, false, true)
     }, function(ply, data)
         RPGM.ResetNickname(ply, data[1])
     end, "Nickname.Reset", "admin")
+
+    RPGM.RegisterCommand("resetnick", {"resetrpname", "resetname", "resetnickname"}, {}, function(ply, data)
+        RPGM.ResetNickname(nil, ply:Name())
+    end)
 end)
