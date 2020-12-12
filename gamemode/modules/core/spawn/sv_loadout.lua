e8ee88b75c27092c98177fd6226651feb402ebd9
TomDotBat <tom@tomdotbat.dev>
2020-12-12 16:16:26 +0000

    Actually give the team specific loadout

--- gamemode/modules/core/spawn/sv_loadout.lua
+++ gamemode/modules/core/spawn/sv_loadout.lua
@@ -1,9 +1,15 @@
 
 function GM:PlayerLoadout(ply)
     self.Sandbox.PlayerLoadout(self, ply)
-    RPGM.CallClassFunction(ply:getTeamClass(), "onPlayerLoadout", ply)
+
+    local teamTbl = ply:getTeamClass()
+    RPGM.CallClassFunction(teamTbl, "onPlayerLoadout", ply)
 
     for k, v in pairs(RPGM.Config.BaseLoadout) do
+        ply:Give(v)
+    end
+
+    for k,v in ipairs(teamTbl:getWeapons()) do
         ply:Give(v)
     end
 end
