a21732cc508c39ffe3c025e52f0d5ddb9f285bbf
Tom O'Sullivan <tom@tomdotbat.dev>
2020-12-29 12:30:12 +0000

    Money drop metamethod

--- gamemode/modules/money/sh_meta.lua
+++ gamemode/modules/money/sh_meta.lua
@@ -34,3 +34,26 @@ end
 function meta:removeMoney(amount, callback)
     RPGM.RemovePlayerMoney(self, amount, callback)
 end
+
+local traceTbl = {}
+
+function meta:dropMoney(amount, callback)
+    return RPGM.RemovePlayerMoney(self, amount, function(success)
+        if not success then return callback(false) end
+
+        self:DoAnimationEvent(ACT_GMOD_GESTURE_ITEM_DROP)
+        timer.Simple(1, function()
+            if not IsValid(self) then return end
+
+            traceTbl.start = self:EyePos()
+            traceTbl.endpos = traceTbl.start + ply:GetAimVector() * 85
+            traceTbl.filter = ply
+
+            local tr = util.TraceLine(traceTbl)
+            local ent = RPGM.SpawnMoney(tr.HitPos, amount)
+            RPGM.PlaceEntity(ent, tr, ply)
+
+            hook.Call("RPGM.PlayerDroppedMoney", nil, ply, ent, amount)
+        end)
+    end)
+end
