0ce81ad0eea4942c077f6849e00ea742b91a8d83
Tom O'Sullivan <tom@tomdotbat.dev>
2020-12-30 15:13:11 +0000

    Command color and range config

--- gamemode/config/modules/sh_commands.lua
+++ gamemode/config/modules/sh_commands.lua
@@ -1,2 +1,8 @@
 
 RPGM.Config.LOOCRange = 550
+RPGM.Config.ActionRange = 550
+
+RPGM.Config.OOCTagCol = Color(52, 157, 209)
+RPGM.Config.LOOCTagCol = Color(46, 138, 184)
+RPGM.Config.PMTagCol = Color(191, 58, 55)
+RPGM.Config.ActionCol = Color(235, 235, 235)
