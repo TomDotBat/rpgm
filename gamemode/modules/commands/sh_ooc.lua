3c6405308cc9301c1c4c12b36bd3c8ffe3fd9c1a
Tom O'Sullivan <tom@tomdotbat.dev>
2020-12-30 15:12:07 +0000

    Add an OOC command shortcut

--- gamemode/modules/commands/sh_ooc.lua
+++ gamemode/modules/commands/sh_ooc.lua
@@ -1,14 +1,24 @@
 
 hook.Add("RPGM.RegisterCommands", "RPGM.OOCCommands", function()
-    RPGM.RegisterCommand("ooc", {"/", "oc"}, {
+    local oocCommand = RPGM.RegisterCommand("ooc", {"/", "oc"}, {
         RPGM.Classes.TextArgument("Message", false, nil, false, true)
     }, function(ply, data)
-        RPGM.TalkToAll(ply, data[1])
+        RPGM.TalkToAll(ply, data[1], nil, RPGM.Config.OOCTagCol, "[OOC]")
     end)
 
-    RPGM.RegisterCommand("looc", {"loc"}, {
+    hook.Add("RPGM.PreCommandCheck", "RPGM.OOCShortcuts", function(ply, text)
+        if not string.StartWith(text, "//") then return end
+
+        local length = #text
+        oocCommand:execute(length <= 2 and "" or string.Right(text, length - 2), ply, RPGM.CommandCallback)
+
+        return ""
+    end)
+
+    RPGM.RegisterCommand("looc", {"/l", "loc"}, {
         RPGM.Classes.TextArgument("Message", false, nil, false, true)
     }, function(ply, data)
-        RPGM.TalkToRange(ply, data[1], RPGM.Config.LOOCRange)
+        RPGM.TalkToRange(ply, data[1], RPGM.Config.LOOCRange, nil, RPGM.Config.LOOCTagCol, "[LOOC]")
+        RPGM.TalkToPlayer(ply, ply, data[1], RPGM.Config.LOOCTagCol, "[LOOC]")
     end)
 end)
