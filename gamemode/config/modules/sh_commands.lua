f47a6ea7aef79f11351579312ab265243c97679d
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-25 11:41:02 +0000

    Add whisper and yell command config

--- gamemode/config/modules/sh_commands.lua
+++ gamemode/config/modules/sh_commands.lua
@@ -1,8 +1,12 @@
 
 RPGM.Config.LOOCRange = 550
-RPGM.Config.ActionRange = 550
+RPGM.Config.ActionRange = 250
+RPGM.Config.YellRange = 550
+RPGM.Config.WhisperRange = 100
 
 RPGM.Config.OOCTagCol = Color(52, 157, 209)
 RPGM.Config.LOOCTagCol = Color(46, 138, 184)
 RPGM.Config.PMTagCol = Color(191, 58, 55)
 RPGM.Config.ActionCol = Color(235, 235, 235)
+RPGM.Config.YellTagCol = Color(227, 203, 25)
+RPGM.Config.WhisperTagCol = Color(189, 189, 189)
