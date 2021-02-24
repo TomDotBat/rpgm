1d01bc71c6341fa1fad57a4c2fc3e68b6fb04a8c
Tom O'Sullivan <tom@tomdotbat.dev>
2021-02-24 13:18:27 +0000

    Add a config for notification sounds (#13)

--- gamemode/config/core/cl_config.lua
+++ gamemode/config/core/cl_config.lua
@@ -7,4 +7,11 @@ RPGM.Config.ChatMessageColor = Color(255, 255, 255, 255)
 RPGM.Config.ChatMessageDeadColor = Color(255, 200, 200, 255)
 
 --Misc Settings
-RPGM.Config.EnableDecalCleaner = true 
+RPGM.Config.EnableDecalCleaner = true
+RPGM.Config.NotificationSounds = {
+    [NOTIFY_GENERIC] = "buttons/lightswitch2.wav",
+    [NOTIFY_ERROR] = "buttons/lightswitch2.wav",
+    [NOTIFY_UNDO] = "buttons/lightswitch2.wav",
+    [NOTIFY_HINT] = "buttons/lightswitch2.wav",
+    [NOTIFY_CLEANUP] = "buttons/lightswitch2.wav"
+}
