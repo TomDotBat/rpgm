e82208d0cd12daaa8c2ae90c64726b98063050b5
Tom O'Sullivan <tom@tomdotbat.dev>
2021-03-04 18:26:05 +0000

    Use a shared font system

--- gamemode/modules/ui/common/cl_fonts.lua
+++ gamemode/modules/ui/common/cl_fonts.lua
@@ -1,27 +1,73 @@
 
-function RPGM.RegisterFontUnscaled(name, font, size, weight)
-    surface.CreateFont("RPGM." .. name, {
-        font = font,
-        size = size,
-        weight = weight or 500,
-        antialias = true
-    })
-end
+RPGM.RegisteredFonts = RPGM.RegisteredFonts or {}
+local registeredFonts = RPGM.RegisteredFonts
 
-local scaledFonts = {}
+do
+    RPGM.SharedFonts = RPGM.SharedFonts or {}
+    local sharedFonts = RPGM.SharedFonts
 
-function RPGM.RegisterFont(name, font, size, weight)
-    scaledFonts[name] = {
-        font = font,
-        size = size,
-        weight = weight
-    }
+    function RPGM.RegisterFontUnscaled(name, font, size, weight)
+        weight = weight or 500
 
-    RPGM.RegisterFontUnscaled(name, font, RPGM.Scale(size), weight)
-end
+        local identifier = font .. size .. ":" .. weight
 
-hook.Add("OnScreenSizeChanged", "RPGM.ReRegisterFonts", function()
-    for k,v in pairs(scaledFonts) do
-        RPGM.RegisterFont(k, v.font, v.size, v.weight)
+        local fontName = "RPGM:" .. identifier
+        registeredFonts[name] = fontName
+
+        if sharedFonts[identifier] then return end
+        sharedFonts[identifier] = true
+
+        surface.CreateFont(fontName, {
+            font = font,
+            size = size,
+            weight = weight,
+            antialias = true
+        })
     end
-end)
+end
+
+do
+    RPGM.ScaledFonts = RPGM.ScaledFonts or {}
+    local scaledFonts = RPGM.ScaledFonts
+
+    function RPGM.RegisterFont(name, font, size, weight)
+        scaledFonts[name] = {
+            font = font,
+            size = size,
+            weight = weight
+        }
+
+        RPGM.RegisterFontUnscaled(name, font, RPGM.Scale(size), weight)
+    end
+
+    hook.Add("OnScreenSizeChanged", "RPGM.ReRegisterFonts", function()
+        for k,v in pairs(scaledFonts) do
+            RPGM.RegisterFont(k, v.font, v.size, v.weight)
+        end
+    end)
+end
+
+do
+    local setFont = surface.SetFont
+    local function setRPGMFont(font)
+        local RPGMFont = registeredFonts[font]
+        if RPGMFont then
+            setFont(RPGMFont)
+            return
+        end
+
+        setFont(font)
+    end
+
+    RPGM.SetFont = setRPGMFont
+
+    local getTextSize = surface.GetTextSize
+    function RPGM.GetTextSize(text, font)
+        if font then setRPGMFont(font) end
+        return getTextSize(text)
+    end
+
+    function RPGM.GetRealFont(font)
+        return registeredFonts[font]
+    end
+end
