aef28e0ee72423f243242489b16aea8ec76591a1
Tom O'Sullivan <tom@tomdotbat.dev>
2021-03-04 18:30:52 +0000

    Fix variable references in the progress bar element

--- gamemode/modules/ui/elements/cl_progress_bar.lua
+++ gamemode/modules/ui/elements/cl_progress_bar.lua
@@ -7,21 +7,21 @@ AccessorFunc(PANEL, "cBackgroundColor", "BackgroundColor")
 AccessorFunc(PANEL, "cForegroundColor", "ForegroundColor")
 
 function PANEL:Init()
-    self.Progress = 0
-    self.BackgroundColor = color_black
-    self.ForegroundColor = color_white
+    self.nProgress = 0
+    self.cBackgroundColor = color_black
+    self.cForegroundColor = color_white
 end
 
 local lerp = Lerp
 function PANEL:Think()
-    self.SmoothProg = lerp(FrameTime() * 5, self.SmoothProg, self.Progress)
+    self.SmoothProg = lerp(FrameTime() * 5, self.SmoothProg, self.nProgress)
 end
 
 function PANEL:Paint(w, h)
-    surface.SetDrawColor(self.BackgroundColor)
+    surface.SetDrawColor(self.cBackgroundColor)
     surface.DrawRect(0, 0, w, h)
 
-    surface.SetDrawColor(self.ForegroundColor)
+    surface.SetDrawColor(self.cForegroundColor)
 
     local width = w * self.SmoothProg
     if self.RightToLeft then
