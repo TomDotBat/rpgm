4ee3007dcda3a87657cb707e28cb8b918d444e33
Tom O'Sullivan <tom@tomdotbat.dev>
2021-03-09 12:16:48 +0000

    Remove AccessorFunc()

--- gamemode/modules/core/util/sh_util.lua
+++ gamemode/modules/core/util/sh_util.lua
@@ -1,25 +1,4 @@
 
-function RPGM.Util.AccessorFunc(tbl, varName, functionName, forceType)
-    tbl["get" .. functionName] = function(self) return self[varName] end
-
-    if forceType == FORCE_STRING then
-        tbl["set" .. functionName] = function(self, val) self[varName] = tostring(val) end
-        return
-    end
-
-    if forceType == FORCE_NUMBER then
-        tbl["set" .. functionName] = function(self, val) self[varName] = tonumber(val) end
-        return
-    end
-
-    if forceType == FORCE_BOOL then
-        tbl["set" .. functionName] = function(self, val) self[varName] = tobool(val) end
-        return
-    end
-
-    tbl["set" .. functionName] = function(self, val) self[varName] = val end
-end
-
 function RPGM.Util.IsSteamID(str)
     return isstring(str) and str:match("^STEAM_[0-5]:[0-1]:[0-9]+$") ~= nil
 end
