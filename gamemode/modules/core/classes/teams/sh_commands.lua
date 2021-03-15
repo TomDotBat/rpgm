55c53115403552c9af421024b88be58db089b4be
Tom O'Sullivan <tom@tomdotbat.dev>
2021-03-15 09:45:49 +0000

    Fix team change error string replacement

--- gamemode/modules/core/classes/teams/sh_commands.lua
+++ gamemode/modules/core/classes/teams/sh_commands.lua
@@ -14,12 +14,12 @@ hook.Add("RPGM.RegisterCommands", "RPGM.TeamCommand", function()
 
         local teamTbl = ply:getTeamClass()
         if teamTbl and cmd == teamTbl:getCommand() then
-            RPGM.MessagePlayer(ply, lang:getString("alreadyATeam", {teamName = teamName}), errorMessageCol)
+            RPGM.MessagePlayer(ply, lang:getString("alreadyATeam", {teamName = cmd}), errorMessageCol)
             return
         end
 
         if ply:joinTeam(cmd) then
-            RPGM.MessagePlayer(ply, lang:getString("teamNotFound", {teamName = teamName}), errorMessageCol)
+            RPGM.MessagePlayer(ply, lang:getString("teamNotFound", {teamName = cmd}), errorMessageCol)
         end
     end)
 end)
